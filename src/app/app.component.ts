7f14539587f8ea1fa24cdec0658452b96c4cb2ef
dinod <dinodniluksha@gmail.com>
2022-08-09 18:20:33 +0530

    added dynamic meal form - Breakfast

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,224 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, SimpleChanges } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { MatStepper } from '@angular/material/stepper';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'meals-survey';
+  mealsFromBording = true;
+  canteenFoods = ['Hoppers', 'Milk Rice', 'Rice & Curry'];
+  orderingFoods = ['Hoppers', 'Milk Rice', 'Rice & Curry'];
+  isCanteenFoods = false;
+  isOrderingFoods = false;
+  mealGetFrom: string[] = [];
+  pickedCanteenFoods: string[] = [];
+  pickedOrderingFoods: string[] = [];
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.initializeGoogleSheet();
+  }
+
+  initializeGoogleSheet() {
+    console.log('Calling method: initializeGoogleSheet()');
+  }
+  mealFrom: FormGroup = this.fb.group({
+    boarding: [false],
+    canteen: [false],
+    ordering: [false],
+  });
+
+  firstFormGroup: FormGroup = this.fb.group({
+    mealFrom: [[], ValidateMealFrom],
+  });
+
+  secondFormGroup = this.fb.group({
+    mealFrom: ['', Validators.required],
+  });
+  thirdFormGroup = this.fb.group({
+    mealFrom: ['', Validators.required],
+  });
+  forthFormGroup = this.fb.group({
+    email: ['', Validators.required],
+    comment: ['', Validators.required],
+  });
+
+  firstNext(stepper: MatStepper) {
+    if (this.firstFormGroup.valid) {
+      stepper.next();
+    }
+  }
+  secondNext(stepper: MatStepper) {
+    if (this.secondFormGroup.valid) {
+      stepper.next();
+    }
+  }
+  thirdNext(stepper: MatStepper) {
+    if (this.thirdFormGroup.valid) {
+      stepper.next();
+    }
+  }
+
+  change() {
+    console.log(this.firstFormGroup.value);
+  }
+
+  handleChange(event: any) {
+    let { boarding, canteen, ordering } = this.mealFrom.value;
+
+    this.isCanteenFoods = canteen;
+    this.isOrderingFoods = ordering;
+    let key = event.source.value;
+    console.log(key);
+    console.log(key + ' :' + JSON.stringify(this.mealFrom.value));
+
+    if (event.checked) {
+      if (key === 'canteen') {
+        this.firstFormGroup.addControl(
+          'pickedCanteenFoods',
+          new FormControl([], ValidateMealFrom)
+        );
+        this.firstFormGroup.addControl(key, new FormGroup({}));
+      }
+      if (key === 'ordering') {
+        this.firstFormGroup.addControl(
+          'pickedOrderingFoods',
+          new FormControl([], ValidateMealFrom)
+        );
+        this.firstFormGroup.addControl(key, new FormGroup({}));
+        this.firstFormGroup.addControl(
+          'collectingTime',
+          new FormControl('', Validators.required)
+        );
+      }
+      this.mealGetFrom.push(key);
+      this.firstFormGroup.controls['mealFrom'].setValue(this.mealGetFrom);
+      if (key !== 'boarding') {
+        this.mealsFromBording = false;
+      }
+    }
+    if (!canteen && !ordering) {
+      this.mealsFromBording = true;
+    }
+
+    if (key == 'boarding' && !boarding) {
+      this.firstFormGroup.removeControl('boarding');
+      this.removeItemByName(key, this.mealGetFrom);
+      this.firstFormGroup.controls['mealFrom'].setValue(this.mealGetFrom);
+    }
+    if (key == 'canteen' && !canteen) {
+      this.firstFormGroup.removeControl('canteen');
+      this.firstFormGroup.removeControl('pickedCanteenFoods');
+      //this.firstFormGroup.updateValueAndValidity;
+      this.removeItemByName(key, this.mealGetFrom);
+      this.firstFormGroup.controls['mealFrom'].setValue(this.mealGetFrom);
+      this.pickedCanteenFoods = [];
+    }
+    if (key == 'ordering' && !ordering) {
+      this.firstFormGroup.removeControl('ordering');
+      this.firstFormGroup.removeControl('pickedOrderingFoods');
+      this.firstFormGroup.removeControl('collectingTime');
+
+      this.removeItemByName(key, this.mealGetFrom);
+      this.firstFormGroup.controls['mealFrom'].setValue(this.mealGetFrom);
+      this.pickedOrderingFoods = [];
+    }
+    console.log(this.firstFormGroup);
+  }
+
+  showCanteenOptions(event: any) {
+    console.log(event.source.value + ' : ' + event.checked); //true or false
+    let key: string = event.source.value;
+    let canteen = this.firstFormGroup.get('canteen') as FormGroup;
+    if (event.checked) {
+      //need to add form controller
+      canteen.addControl(
+        key,
+        this.fb.control('', [
+          Validators.required,
+          Validators.pattern(/^-?(0|[1-9]\d*)?$/),
+        ])
+      );
+      this.pickedCanteenFoods.push(key);
+    } else {
+      //need to remove existing form controller
+      canteen.removeControl(key);
+      this.removeItemByName(key, this.pickedCanteenFoods);
+    }
+    console.log(this.firstFormGroup);
+    console.log(this.pickedCanteenFoods);
+    this.firstFormGroup.controls['pickedCanteenFoods'].setValue(
+      this.pickedCanteenFoods
+    );
+  }
+
+  showOrderingOptions(event: any) {
+    console.log(event.source.value + ' : ' + event.checked); //true or false
+    let key: string = event.source.value;
+    let ordering = this.firstFormGroup.get('ordering') as FormGroup;
+    if (event.checked) {
+      //need to add form controller
+      ordering.addControl(
+        key,
+        this.fb.control('', [
+          Validators.required,
+          Validators.pattern(/^-?(0|[1-9]\d*)?$/),
+        ])
+      );
+      this.pickedOrderingFoods.push(key);
+    } else {
+      //need to remove existing form controller
+      ordering.removeControl(key);
+      this.removeItemByName(key, this.pickedOrderingFoods);
+    }
+    console.log(this.firstFormGroup);
+    console.log(this.pickedOrderingFoods);
+    this.firstFormGroup.controls['pickedOrderingFoods'].setValue(
+      this.pickedOrderingFoods
+    );
+  }
+
+  submit(): void {
+    if (this.forthFormGroup.valid) {
+      console.log(
+        'Submiting...' + JSON.stringify(this.firstFormGroup.getRawValue())
+      );
+      console.log(
+        'Submiting...' + JSON.stringify(this.secondFormGroup.getRawValue())
+      );
+      console.log(
+        'Submiting...' + JSON.stringify(this.thirdFormGroup.getRawValue())
+      );
+      console.log(
+        'Submiting...' + JSON.stringify(this.forthFormGroup.getRawValue())
+      );
+    }
+  }
+
+  removeItemByName(item: string, items: string[]) {
+    const index = items.indexOf(item);
+    if (index !== -1) {
+      items.splice(index, 1);
+    }
+  }
+}
+
+function ValidateMealFrom(
+  control: AbstractControl
+): { [key: string]: any } | null {
+  if (control.value.length < 1) {
+    return { nonValidateMealFrom: true };
+  }
+  return null;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatGridListModule,
+    MatCheckboxModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
