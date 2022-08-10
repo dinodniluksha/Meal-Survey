d12b9d978c53353fc3c5763deb0f1051a75027f8
dinod <dinodniluksha@gmail.com>
2022-08-10 19:59:26 +0530

    changed to different firebase project with github CI/CD pipeline

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAx7ab8V16z8-mExblostSw1sjFTHc1REc',
+    authDomain: 'meal-survey-bc967.firebaseapp.com',
+    databaseURL:
+      'https://meal-survey-bc967-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'meal-survey-bc967',
+    storageBucket: 'meal-survey-bc967.appspot.com',
+    messagingSenderId: '791304591771',
+    appId: '1:791304591771:web:e3a6a4d84d703305e1693e',
+    measurementId: 'G-59S4XF53LR',
+  },
 };
 
 /*
