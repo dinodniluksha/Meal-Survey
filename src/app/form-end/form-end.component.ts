205e069b8f09e767e12dfbd3914d995668abaf81
dinod <dinodniluksha@gmail.com>
2022-08-20 11:19:14 +0530

    completed adding end-page

---
+++ src/app/form-end/form-end.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-end',
+  templateUrl: './form-end.component.html',
+  styleUrls: ['./form-end.component.css']
+})
+export class FormEndComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
